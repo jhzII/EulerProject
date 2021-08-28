87d4fd9ded3cdccdb0ff8c1b8063edd9080c1659
Boris Gomziakov <jhzll@yandex.ru>
2021-08-28 20:05:33 +0300

    Task 24 v2.

--- Task24.py
+++ Task24.py
@@ -5,7 +5,7 @@
 # Словарные перестановки из цифр 0, 1 и 2 представлены ниже:
 # 012   021   102   120   201   210
 # Какова миллионная словарная перестановка из цифр 0, 1, 2, 3, 4, 5, 6, 7, 8 и 9?
-
+import math
 from typing import List
 
 
@@ -17,10 +17,23 @@ def f(digits: List[str],  result: List[str], start: str = ''):
             f(digits[0:i]+digits[i+1:], result, start + digits[i])
 
 
+def g(digits: List[str], index, start: str = ''):
+    length = len(digits)
+    if length == 1:
+        return start + digits[0]
+
+    quantity = math.factorial(length-1)
+    next_digit = index // quantity
+    index = index % quantity
+
+    return g(digits[0:next_digit]+digits[next_digit+1:], index, start + digits[next_digit])
+
+
 if __name__ == '__main__':
     DIGITS = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
-    arr = []
-    f(DIGITS, arr)
+    # arr = []
+    # f(DIGITS, arr)
+    # print(arr[999999])
 
-    print(arr[999999])
+    print(g(DIGITS, 1000000-1))
     # 2783915460
